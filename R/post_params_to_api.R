cc465a287bc956a30eb344d3f9c3ba438a4b5926
Fran Barton <francis.barton@nhs.net>
2026-03-02 13:27:11 +0000

    :stethoscope: Use check_getenv function

--- R/post_params_to_api.R
+++ R/post_params_to_api.R
@@ -1,14 +1,22 @@
-post_params_to_api <- function(params_json) {
-  req <- httr2::request(Sys.getenv("NHP_API_URI"))
+  req <- httr2::request(check_getenv("NHP_API_URI"))
   req |>
     httr2::req_url_path_append("api") |>
     httr2::req_url_path_append("run_model") |>
     httr2::req_url_query(
-      app_version = Sys.getenv("NHP_APP_VERSION"),
-      code = Sys.getenv("NHP_API_KEY")
+      app_version = check_getenv("NHP_VERSION"),
+      code = check_getenv("NHP_API_KEY")
     ) |>
     httr2::req_body_json(params_json) |>
     httr2::req_perform() |>
     httr2::resp_check_status() |>
     httr2::resp_body_json()
+}
+
+
+check_getenv <- function(var) {
+  if (is.na(Sys.getenv(var, NA))) {
+    cli::cli_abort("Environment variable {.var {var}} is not set")
+  } else {
+    Sys.getenv(var)
+  }
 }
